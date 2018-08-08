9d10abe00a50459a7e40c8dbe7b37dd9ef8bd75e
DaveHalvorsen <daviqd.halvorsen@gmail.com>
2018-08-08 11:03:58 -0700

    sum of odd numbers between a and b

--- Python_Village/sum_odd_numbers.py
+++ Python_Village/sum_odd_numbers.py
@@ -5,6 +5,17 @@
 # 100 200
 # Sample Output
 # 7500
-current_total = 0
+
 def sum_odd_numbers(a, b):
-    while(a < b):
+    current_total = 0
+    while(a <= b):
+        print("a is currently: " + str(a))
+        if a % 2 == 1:
+            current_total = current_total + a
+            print(str(a) + " is an odd number!")
+        a = a + 1
+    print("Sum of odd numbers between " + str(a) + " and " + str(b))
+    print("Is equal to: " + str(current_total))
+a = 4305
+b = 8525
+sum_odd_numbers(a, b)
